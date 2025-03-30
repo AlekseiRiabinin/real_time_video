ea8ccbe41a9aeec41406d2dae63991c834747c84
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2025-03-30 20:09:00 +0300

    Update fs2-app for reading multiple video files.

--- apps/fs2-client/build.sbt
+++ apps/fs2-client/build.sbt
@@ -1,16 +1,34 @@
 import sbtassembly.AssemblyPlugin.autoImport._
+import sbtassembly.MergeStrategy
 
 lazy val root = (project in file("."))
   .settings(
     name := "FS2Client",
+    version := "0.1.0-SNAPSHOT",
     scalaVersion := "3.3.4",
+    
+    // Assembly plugin settings
+    assembly / mainClass := Some("FS2Client"),
+    assembly / assemblyJarName := "fs2-client-assembly-0.1.0-SNAPSHOT.jar",
+    assembly / assemblyOption := (assembly / assemblyOption).value.copy(includeScala = true),
+    
+    // Dependencies
     libraryDependencies ++= Seq(
       "com.github.fd4s" %% "fs2-kafka" % "3.6.0",
       "org.typelevel" %% "cats-effect" % "3.5.4",
       "org.apache.kafka" % "kafka-clients" % "3.7.0",
-      "org.apache.hadoop" % "hadoop-client" % "3.3.6" exclude("javax.activation", "activation") exclude("javax.xml.bind", "jaxb-api"),
-      "org.apache.hadoop" % "hadoop-common" % "3.3.6" exclude("javax.activation", "activation") exclude("javax.xml.bind", "jaxb-api"),
-      "org.apache.hadoop" % "hadoop-hdfs" % "3.3.6" exclude("javax.activation", "activation") exclude("javax.xml.bind", "jaxb-api"),
+      "org.apache.hadoop" % "hadoop-client" % "3.3.6" excludeAll(
+        ExclusionRule("javax.activation", "activation"),
+        ExclusionRule("javax.xml.bind", "jaxb-api")
+      ),
+      "org.apache.hadoop" % "hadoop-common" % "3.3.6" excludeAll(
+        ExclusionRule("javax.activation", "activation"),
+        ExclusionRule("javax.xml.bind", "jaxb-api")
+      ),
+      "org.apache.hadoop" % "hadoop-hdfs" % "3.3.6" excludeAll(
+        ExclusionRule("javax.activation", "activation"),
+        ExclusionRule("javax.xml.bind", "jaxb-api")
+      ),
       "org.bytedeco" % "opencv" % "4.10.0-1.5.11" exclude("org.bytedeco", "javacpp-presets"),
       "org.bytedeco" % "ffmpeg" % "7.1-1.5.11" exclude("org.bytedeco", "javacpp-presets"),
       "org.bytedeco" % "javacpp" % "1.5.11",
@@ -22,24 +40,42 @@ lazy val root = (project in file("."))
       "io.prometheus" % "simpleclient_httpserver" % "0.16.0",
       "com.typesafe" % "config" % "1.4.3"
     ),
+    
     resolvers ++= Seq(
       "Apache Repository" at "https://repository.apache.org/content/repositories/releases/",
       "Maven Central" at "https://repo1.maven.org/maven2/",
-      "Bytedeco" at "https://repo.bytedeco.org/releases/"
+      "Bytedeco" at "https://repo.bytedeco.org/releases/",
+      "Typelevel Repository" at "https://repo.typelevel.org/stable/"
     ),
+    
+    // Enhanced merge strategy
     assembly / assemblyMergeStrategy := {
-      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+      case PathList("META-INF", "MANIFEST.MF") => MergeStrategy.discard
+      case PathList("META-INF", "services", _*) => MergeStrategy.filterDistinctLines
+      case PathList("META-INF", "versions", _*) => MergeStrategy.first
+      case PathList("META-INF", _*) => MergeStrategy.discard
       case "module-info.class" => MergeStrategy.discard
-      case PathList("version.conf") => MergeStrategy.concat
-      case PathList("org", "opencv", xs @ _*) => MergeStrategy.first
-      case PathList("org", "bytedeco", "javacpp", "linux-x86_64", xs @ _*) => MergeStrategy.first
-      case PathList("javax", "activation", xs @ _*) => MergeStrategy.discard
-      case PathList("javax", "xml", "bind", xs @ _*) => MergeStrategy.discard
-      case PathList("org", "apache", "hadoop", xs @ _*) => MergeStrategy.first
-      case PathList("org", "slf4j", "impl", xs @ _*) => MergeStrategy.first
-      case x =>
-        val oldStrategy = (assembly / assemblyMergeStrategy).value
-        oldStrategy(x)
+      case "reference.conf" => MergeStrategy.concat
+      case "version.conf" => MergeStrategy.concat
+      case PathList("org", "opencv", _*) => MergeStrategy.first
+      case PathList("org", "bytedeco", _*) => MergeStrategy.first
+      case PathList("org", "bytedeco", "javacpp", "linux-x86_64", _*) => MergeStrategy.first
+      case PathList("javax", "inject", _*) => MergeStrategy.first
+      case PathList("javax", "activation", _*) => MergeStrategy.discard
+      case PathList("javax", "xml", "bind", _*) => MergeStrategy.discard
+      case PathList("org", "apache", "hadoop", _*) => MergeStrategy.first
+      case PathList("org", "slf4j", _*) => MergeStrategy.first
+      // FS2/Cats specific cases
+      case PathList("cats", _*) => MergeStrategy.first
+      case PathList("fs2", _*) => MergeStrategy.first
+      case PathList("co", "fs2", _*) => MergeStrategy.first
+      case x => MergeStrategy.first
     },
-    Compile / run / mainClass := Some("FS2Client")
+    
+    // Runtime settings
+    Compile / run / mainClass := Some("FS2Client"),
+    run / fork := true,
+    run / javaOptions ++= Seq(
+      "-Djava.library.path=/usr/local/lib"
+    )
   )
--- apps/fs2-client/src/main/scala/FS2Client.scala
+++ apps/fs2-client/src/main/scala/FS2Client.scala
@@ -1,20 +1,21 @@
 import scala.concurrent.duration._
-import cats.effect.{IO, IOApp, Resource}
+import cats.effect.{IO, IOApp, Resource, Ref, Deferred}
+import cats.effect.unsafe.implicits.global
 import cats.implicits._
 import fs2.kafka._
 import fs2.Stream
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path, FSDataInputStream}
 import java.net.URI
-import org.bytedeco.javacv.{FFmpegFrameGrabber, Java2DFrameConverter}
+import org.bytedeco.javacv.{FFmpegFrameGrabber, Java2DFrameConverter, FFmpegLogCallback}
 import io.prometheus.client.exporter.HTTPServer
 import io.prometheus.client.hotspot.DefaultExports
 import io.prometheus.client.{CollectorRegistry, Counter, Gauge, Histogram}
 import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 
 
 object FS2Client extends IOApp.Simple {
-
   // Constants for labels
   private val APPLICATION_LABEL = "application"
   private val INSTANCE_LABEL = "instance"
@@ -25,10 +26,12 @@ object FS2Client extends IOApp.Simple {
   private val INSTANCE_VALUE = "fs2-client:9083"
   private val JOB_VALUE = "fs2-client"
 
+  private val logger = LoggerFactory.getLogger(getClass)
+
   // Configuration case classes
   case class HdfsConfig(uri: String, videoPath: String)
   case class KafkaConfig(bootstrapServers: String, topic: String)
-  case class VideoConfig(frameWidth: Int, frameHeight: Int, frameRate: Int)
+  case class VideoConfig(frameWidth: Int, frameHeight: Int, frameRate: Int, format: String)
   case class AppConfig(hdfs: HdfsConfig, kafka: KafkaConfig, video: VideoConfig)
 
   // Load configuration from application.conf
@@ -45,7 +48,8 @@ object FS2Client extends IOApp.Simple {
     VideoConfig(
       frameWidth = config.getInt("video.frameWidth"),
       frameHeight = config.getInt("video.frameHeight"),
-      frameRate = config.getInt("video.frameRate")
+      frameRate = config.getInt("video.frameRate"),
+      format = config.getString("video.format")
     )
   )
 
@@ -55,6 +59,7 @@ object FS2Client extends IOApp.Simple {
   require(appConfig.video.frameWidth > 0, "Frame width must be positive")
   require(appConfig.video.frameHeight > 0, "Frame height must be positive")
   require(appConfig.video.frameRate > 0, "Frame rate must be positive")
+  require(appConfig.video.format.nonEmpty, "Video format must not be empty")
 
   // HDFS configuration
   val conf = new Configuration()
@@ -67,16 +72,18 @@ object FS2Client extends IOApp.Simple {
     FileSystem.get(new URI(appConfig.hdfs.uri), conf)
   } catch {
     case ex: Exception =>
-      println(s"Failed to connect to HDFS: ${ex.getMessage}")
+      logger.error(s"Failed to connect to HDFS: ${ex.getMessage}")
       hdfsReadErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
-      System.exit(1) // Exit the program if HDFS connection fails
-      throw ex // This line is unreachable but required for type safety
+      System.exit(1)
+      throw ex
   }
 
   // Kafka Producer Settings
   val producerSettings: ProducerSettings[IO, Array[Byte], Array[Byte]] =
     ProducerSettings[IO, Array[Byte], Array[Byte]]
       .withBootstrapServers(appConfig.kafka.bootstrapServers)
+      .withLinger(100.millis) // Batch small sends
+      .withBatchSize(16384) // 16KB batch size
 
   // Prometheus metrics with labels
   val framesProduced: Counter = Counter.build()
@@ -121,94 +128,166 @@ object FS2Client extends IOApp.Simple {
 
   // Resource for HDFS InputStream
   def hdfsInputStreamResource(videoPath: Path): Resource[IO, FSDataInputStream] =
-    Resource.make(IO(fs.open(videoPath)))(stream => IO(stream.close()))
+    Resource.make(IO(fs.open(videoPath))) { stream =>
+      IO(stream.close()).handleErrorWith(e => 
+        IO(logger.error(s"Error closing HDFS stream: ${e.getMessage}")))
+    }
 
-  // Function to process video frames and send them to Kafka
-  def processVideoFrames(producer: KafkaProducer[IO, Array[Byte], Array[Byte]], videoPath: Path): IO[Unit] = {
-    hdfsInputStreamResource(videoPath).use { hdfsInputStream =>
-      IO.blocking {
-        val grabber = new FFmpegFrameGrabber(hdfsInputStream)
+  // Resource for FFmpeg components
+  def ffmpegResources(
+    inputStream: FSDataInputStream
+  ): Resource[IO, (FFmpegFrameGrabber, Java2DFrameConverter)] = {
+    val grabberResource = Resource.make {
+      IO {
+        val grabber = new FFmpegFrameGrabber(inputStream)
+        grabber.setFormat(appConfig.video.format)
         grabber.setImageWidth(appConfig.video.frameWidth)
         grabber.setImageHeight(appConfig.video.frameHeight)
         grabber.setFrameRate(appConfig.video.frameRate)
         grabber.start()
+        grabber
+      }
+    } { grabber =>
+      IO(grabber.stop()).handleErrorWith(e => 
+        IO(logger.error(s"Error stopping FFmpeg grabber: ${e.getMessage}")))
+    }
 
-        val converter = new Java2DFrameConverter()
+    val converterResource = Resource.make(IO(new Java2DFrameConverter())) { converter =>
+      IO(converter.close()).handleErrorWith(e => 
+        IO(logger.error(s"Error closing frame converter: ${e.getMessage}")))
+    }
 
-        // FS2 Stream to process frames and send them to Kafka
-        val frameStream = Stream.unfold(()) { _ =>
-          val frame = grabber.grab()
-          if (frame != null) {
-            val startTime = System.nanoTime()
-            val bufferedImage = converter.convert(frame)
-            val byteArray = new Array[Byte](bufferedImage.getWidth * bufferedImage.getHeight * 3)
-            val raster = bufferedImage.getRaster
-            raster.getDataElements(0, 0, bufferedImage.getWidth, bufferedImage.getHeight, byteArray)
+    for {
+      grabber <- grabberResource
+      converter <- converterResource
+    } yield (grabber, converter)
+  }
 
-            // Update Prometheus metrics with application label
-            framesProduced.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
-            frameSize.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).set(byteArray.length)
-            frameProductionTime
-              .labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE)
-              .observe((System.nanoTime() - startTime) / 1e9)
+  // Process a single video file using FS2 Stream
+  def processVideoFile(
+    producer: KafkaProducer[IO, Array[Byte], Array[Byte]],
+    videoPath: Path
+  ): IO[Unit] = {
+    hdfsInputStreamResource(videoPath).use { inputStream =>
+      ffmpegResources(inputStream).use { case (grabber, converter) =>
+        Ref[IO].of(0).flatMap { frameCountRef =>
+          Stream.unfoldEval[IO, Unit, Array[Byte]](()) { _ =>
+            IO.blocking {
+              val frame = grabber.grab()
+              if (frame != null) {
+                val startTime = System.nanoTime()
+                try {
+                  val bufferedImage = converter.convert(frame)
+                  val byteArray = new Array[Byte](bufferedImage.getWidth * bufferedImage.getHeight * 3)
+                  bufferedImage.getRaster.getDataElements(
+                    0, 0, bufferedImage.getWidth, bufferedImage.getHeight, byteArray
+                  )
 
-            Some((byteArray, ()))
-          } else {
-            None
+                  // Update metrics
+                  framesProduced.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
+                  frameSize.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).set(byteArray.length)
+                  frameProductionTime
+                    .labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE)
+                    .observe((System.nanoTime() - startTime) / 1e9)
+
+                  // Update frame count and log periodically
+                  frameCountRef.modify { count =>
+                    val newCount = count + 1
+                    if (newCount % 100 == 0) {
+                      logger.debug(s"Processed $newCount frames from ${videoPath.getName}")
+                      System.gc() // Manual GC to prevent OOM
+                    }
+                    (newCount, byteArray)
+                  }.map(byteArray => Some((byteArray, ())))
+                } catch {
+                  case ex: Exception =>
+                    logger.error(s"Error processing frame: ${ex.getMessage}")
+                    frameProductionErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
+                    IO.pure(None)
+                } finally {
+                  frame.close()
+                }
+              } else {
+                IO.pure(None)
+              }
+            }.flatten
           }
-        }
-
-        frameStream
           .evalMap { byteArray =>
             val record = ProducerRecord(appConfig.kafka.topic, Array.empty[Byte], byteArray)
             producer.produceOne(record).flatten
-              .flatTap(_ => IO(println("Frame sent to Kafka")))
               .handleErrorWith { ex =>
                 kafkaProducerErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
-                IO(println(s"Error sending frame to Kafka: ${ex.getMessage}"))
+                IO(logger.error(s"Error sending frame to Kafka: ${ex.getMessage}"))
               }
           }
           .compile
           .drain
-      }.flatten
-    }.handleErrorWith { ex =>
-      frameProductionErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
-      IO(println(s"Error processing video: ${ex.getMessage}"))
+        }
+      }.handleErrorWith { ex =>
+        IO(logger.error(s"Error processing video ${videoPath.getName}: ${ex.getMessage}")) >>
+          IO(hdfsReadErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc())
+      }
     }
   }
 
-  // Main loop to process video files
-  def processVideoFiles(producer: KafkaProducer[IO, Array[Byte], Array[Byte]]): IO[Unit] = {
+  // Main processing loop with graceful shutdown
+  def processVideoFiles(
+    producer: KafkaProducer[IO, Array[Byte], Array[Byte]],
+    shutdownSignal: Deferred[IO, Unit]
+  ): IO[Unit] = {
+    def listVideoFiles: IO[List[Path]] = IO {
+      val videoDir = new Path(appConfig.hdfs.videoPath)
+      fs.listStatus(videoDir).toList
+        .filter(_.isFile)
+        .filter(_.getPath.getName.matches("video_\\d{2}\\.mp4"))
+        .sortBy(_.getPath.getName)
+        .map(_.getPath)
+    }
+
+    def processBatch(files: List[Path]): IO[Unit] = {
+      files.parTraverse_(file => 
+        processVideoFile(producer, file)
+          .handleErrorWith(e => 
+            IO(logger.error(s"Failed to process ${file.getName}: ${e.getMessage}")))
+    )}
+
     for {
-      _ <- IO.println("Checking for video files...")
-      videoDir = new Path(appConfig.hdfs.videoPath)
-      videoFiles <- IO(fs.listStatus(videoDir).filter(_.isFile))
-      _ <- if (videoFiles.isEmpty) {
-        IO.println("No video files found. Waiting for 5 seconds...") >> IO.sleep(5.seconds)
+      _ <- IO(logger.info("Starting video processing loop"))
+      files <- listVideoFiles
+      _ <- if (files.isEmpty) {
+        IO(logger.warn("No video files found. Waiting...")) >> IO.sleep(5.seconds)
       } else {
-        IO.println(s"Found ${videoFiles.length} video files. Processing...") >>
-          videoFiles.toList.sortBy(_.getPath.getName).traverse_ { fileStatus =>
-            val videoPath = fileStatus.getPath
-            if (videoPath.getName.matches("video_\\d{2}\\.mp4")) {
-              processVideoFrames(producer, videoPath)
-            } else {
-              IO.unit // Skip files that don't match the pattern
-            }
-          }
+        IO(logger.info(s"Found ${files.size} video files to process")) >> 
+          processBatch(files)
       }
-      _ <- processVideoFiles(producer) // Recursively call to continue processing
+      // Check for shutdown signal before continuing
+      shouldContinue <- shutdownSignal.tryGet.map(_.isEmpty)
+      _ <- if (shouldContinue) processVideoFiles(producer, shutdownSignal) else IO.unit
     } yield ()
   }
 
-  // Main entry point
+  // Main entry point with proper resource cleanup
   override def run: IO[Unit] = {
-    for {
-      _ <- IO(DefaultExports.initialize()) // Initialize Prometheus default metrics
-      _ <- IO(new HTTPServer(9083)) // Start Prometheus HTTP server
-      _ <- IO.println("Prometheus HTTP server started on port 9083")
-      _ <- kafkaProducerResource.use { producer =>
-        processVideoFiles(producer) // Start the main processing loop
+    // Initialize FFmpeg logging
+    IO(FFmpegLogCallback.set()) >>
+    // Initialize Prometheus
+    IO(DefaultExports.initialize()) >>
+    Resource.make(IO(new HTTPServer(9083)))(server => IO(server.stop())).use { _ =>
+      IO(logger.info("Prometheus HTTP server started on port 9083")) >>
+      // Setup shutdown handling
+      Deferred[IO, Unit].flatMap { shutdownSignal =>
+        // Register shutdown hook
+        IO(sys.addShutdownHook {
+          logger.info("Shutdown signal received")
+          shutdownSignal.complete(()).unsafeRunSync()
+        }) >>
+        // Main processing with Kafka producer
+        kafkaProducerResource.use { producer =>
+          processVideoFiles(producer, shutdownSignal)
+            .handleErrorWith(e => 
+              IO(logger.error(s"Fatal error in processing loop: ${e.getMessage}")))
+        }
       }
-    } yield ()
+    }
   }
 }
