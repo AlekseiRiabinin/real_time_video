35cb78d1acd0f15a5c3f811d32ff2af9f4c94747
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2025-03-31 11:05:35 +0300

    Update zio-app for reading multiple video files.

--- apps/zio-client/build.sbt
+++ apps/zio-client/build.sbt
@@ -1,18 +1,36 @@
 import sbtassembly.AssemblyPlugin.autoImport._
+import sbtassembly.MergeStrategy
 
 lazy val root = (project in file("."))
   .settings(
     name := "ZIOClient",
+    version := "0.1.0-SNAPSHOT",
     scalaVersion := "3.3.4",
+    
+    // Assembly plugin settings
+    assembly / mainClass := Some("ZIOClient"),
+    assembly / assemblyJarName := "zio-client-assembly-0.1.0-SNAPSHOT.jar",
+    assembly / assemblyOption := (assembly / assemblyOption).value.copy(includeScala = true),
+    
+    // Dependencies
     libraryDependencies ++= Seq(
       "dev.zio" %% "zio" % "2.1.15",
       "dev.zio" %% "zio-kafka" % "2.10.0",
       "dev.zio" %% "zio-config" % "3.0.7",
       "dev.zio" %% "zio-config-typesafe" % "3.0.7",
       "dev.zio" %% "zio-config-magnolia" % "3.0.7",
-      "org.apache.hadoop" % "hadoop-client" % "3.3.6" exclude("javax.activation", "activation") exclude("javax.xml.bind", "jaxb-api"),
-      "org.apache.hadoop" % "hadoop-common" % "3.3.6" exclude("javax.activation", "activation") exclude("javax.xml.bind", "jaxb-api"),
-      "org.apache.hadoop" % "hadoop-hdfs" % "3.3.6" exclude("javax.activation", "activation") exclude("javax.xml.bind", "jaxb-api"),
+      "org.apache.hadoop" % "hadoop-client" % "3.3.6" excludeAll(
+        ExclusionRule("javax.activation", "activation"),
+        ExclusionRule("javax.xml.bind", "jaxb-api")
+      ),
+      "org.apache.hadoop" % "hadoop-common" % "3.3.6" excludeAll(
+        ExclusionRule("javax.activation", "activation"),
+        ExclusionRule("javax.xml.bind", "jaxb-api")
+      ),
+      "org.apache.hadoop" % "hadoop-hdfs" % "3.3.6" excludeAll(
+        ExclusionRule("javax.activation", "activation"),
+        ExclusionRule("javax.xml.bind", "jaxb-api")
+      ),
       "org.bytedeco" % "opencv" % "4.10.0-1.5.11" exclude("org.bytedeco", "javacpp-presets"),
       "org.bytedeco" % "ffmpeg" % "7.1-1.5.11" exclude("org.bytedeco", "javacpp-presets"),
       "org.bytedeco" % "javacpp" % "1.5.11",
@@ -23,24 +41,43 @@ lazy val root = (project in file("."))
       "io.prometheus" % "simpleclient_hotspot" % "0.16.0",
       "io.prometheus" % "simpleclient_httpserver" % "0.16.0"
     ),
+    
     resolvers ++= Seq(
       "Apache Repository" at "https://repository.apache.org/content/repositories/releases/",
       "Maven Central" at "https://repo1.maven.org/maven2/",
-      "Bytedeco" at "https://repo.bytedeco.org/releases/"
+      "Bytedeco" at "https://repo.bytedeco.org/releases/",
+      "ZIO Repository" at "https://repo1.maven.org/maven2/dev/zio/"
     ),
+    
+    // Enhanced merge strategy
     assembly / assemblyMergeStrategy := {
-      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+      case PathList("META-INF", "MANIFEST.MF") => MergeStrategy.discard
+      case PathList("META-INF", "services", _*) => MergeStrategy.filterDistinctLines
+      case PathList("META-INF", "versions", _*) => MergeStrategy.first
+      case PathList("META-INF", _*) => MergeStrategy.discard
       case "module-info.class" => MergeStrategy.discard
-      case PathList("version.conf") => MergeStrategy.concat
-      case PathList("org", "opencv", xs @ _*) => MergeStrategy.first
-      case PathList("org", "bytedeco", "javacpp", "linux-x86_64", xs @ _*) => MergeStrategy.first
-      case PathList("javax", "activation", xs @ _*) => MergeStrategy.discard
-      case PathList("javax", "xml", "bind", xs @ _*) => MergeStrategy.discard
-      case PathList("org", "apache", "hadoop", xs @ _*) => MergeStrategy.first
-      case PathList("org", "slf4j", "impl", xs @ _*) => MergeStrategy.first
-      case x =>
-        val oldStrategy = (assembly / assemblyMergeStrategy).value
-        oldStrategy(x)
+      case "reference.conf" => MergeStrategy.concat
+      case "version.conf" => MergeStrategy.concat
+      case PathList("org", "opencv", _*) => MergeStrategy.first
+      case PathList("org", "bytedeco", _*) => MergeStrategy.first
+      case PathList("org", "bytedeco", "javacpp", "linux-x86_64", _*) => MergeStrategy.first
+      case PathList("javax", "inject", _*) => MergeStrategy.first
+      case PathList("javax", "activation", _*) => MergeStrategy.discard
+      case PathList("javax", "xml", "bind", _*) => MergeStrategy.discard
+      case PathList("org", "apache", "hadoop", _*) => MergeStrategy.first
+      case PathList("org", "slf4j", _*) => MergeStrategy.first
+      // ZIO-specific cases
+      case PathList("dev", "zio", _*) => MergeStrategy.first
+      case PathList("zio", _*) => MergeStrategy.first
+      case x => MergeStrategy.first
     },
-    Compile / run / mainClass := Some("ZIOClient")
+    
+    // Runtime settings
+    Compile / run / mainClass := Some("ZIOClient"),
+    run / fork := true,
+    run / javaOptions ++= Seq(
+      "-Djava.library.path=/usr/local/lib",
+      "-Xmx2G",  // Increased heap size for video processing
+      "-XX:+UseG1GC"  // Recommended GC for ZIO applications
+    )
   )
--- apps/zio-client/src/main/scala/ZIOClient.scala
+++ apps/zio-client/src/main/scala/ZIOClient.scala
@@ -1,22 +1,25 @@
 import zio._
 import zio.kafka.producer._
+import zio.kafka.producer.Producer
 import zio.kafka.serde._
 import zio.config._
 import zio.config.magnolia._
 import zio.config.typesafe._
 import zio.stream.ZStream
-import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path, FSDataInputStream}
+import org.apache.kafka.clients.producer.ProducerRecord
 import java.net.URI
-import org.bytedeco.javacv.{FFmpegFrameGrabber, Java2DFrameConverter}
+import org.bytedeco.javacv.{FFmpegFrameGrabber, Java2DFrameConverter, FFmpegLogCallback}
 import io.prometheus.client.exporter.HTTPServer
 import io.prometheus.client.hotspot.DefaultExports
 import io.prometheus.client.{CollectorRegistry, Counter, Gauge, Histogram}
+import com.typesafe.config.ConfigFactory
+import org.slf4j.{Logger, LoggerFactory}
+import java.util.concurrent.atomic.AtomicInteger
 
 
 object ZIOClient extends ZIOAppDefault {
-
   // Constants for labels
   private val APPLICATION_LABEL = "application"
   private val INSTANCE_LABEL = "instance"
@@ -30,7 +33,7 @@ object ZIOClient extends ZIOAppDefault {
   // Configuration case classes
   case class HdfsConfig(uri: String, videoPath: String)
   case class KafkaConfig(bootstrapServers: String, topic: String)
-  case class VideoConfig(frameWidth: Int, frameHeight: Int, frameRate: Int)
+  case class VideoConfig(frameWidth: Int, frameHeight: Int, frameRate: Int, format: String)
   case class AppConfig(hdfs: HdfsConfig, kafka: KafkaConfig, video: VideoConfig)
 
   // Load configuration from application.conf
@@ -76,122 +79,204 @@ object ZIOClient extends ZIOAppDefault {
     .labelNames(APPLICATION_LABEL, INSTANCE_LABEL, JOB_LABEL)
     .register()
 
-  // Function to process video frames and send them to Kafka
-  def processVideoFrames(producer: Producer, videoPath: Path, config: AppConfig): ZIO[Any, Throwable, Unit] = {
-    for {
-      // Open HDFS input stream
-      hdfsInputStream <- ZIO.attemptBlocking {
+  // Resource for HDFS FileSystem
+  def fileSystem(config: AppConfig): ZIO[Scope, Throwable, FileSystem] =
+    ZIO.acquireRelease {
+      ZIO.attemptBlocking {
         val conf = new Configuration()
         conf.set("fs.defaultFS", config.hdfs.uri)
         conf.addResource(new Path("/etc/hadoop/core-site.xml"))
         conf.addResource(new Path("/etc/hadoop/hdfs-site.xml"))
-        val fs = FileSystem.get(new URI(config.hdfs.uri), conf)
-        fs.open(videoPath)
+        FileSystem.get(new URI(config.hdfs.uri), conf)
       }.catchAll { ex =>
         hdfsReadErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
         ZIO.fail(ex)
       }
-      _ <- ZIO.attempt(println(s"Video file opened from HDFS: ${videoPath.getName}"))
+    } { fs =>
+      ZIO.attemptBlocking(fs.close()).orDie
+    }
 
-      // Initialize FFmpegFrameGrabber with the HDFS input stream
-      grabber <- ZIO.attemptBlocking {
-        val grabber = new FFmpegFrameGrabber(hdfsInputStream)
+  // Resource for HDFS InputStream
+  def hdfsInputStream(fs: FileSystem, videoPath: Path): ZIO[Scope, Throwable, FSDataInputStream] =
+    ZIO.acquireRelease(
+      ZIO.attemptBlocking(fs.open(videoPath))
+    )(stream => ZIO.attemptBlocking(stream.close()).orDie)
+
+  // Resource for FFmpeg components
+  def ffmpegResources(
+    inputStream: FSDataInputStream,
+    config: AppConfig
+  ): ZIO[Scope, Throwable, (FFmpegFrameGrabber, Java2DFrameConverter)] = {
+    val grabberResource = ZIO.acquireRelease {
+      ZIO.attemptBlocking {
+        val grabber = new FFmpegFrameGrabber(inputStream)
+        grabber.setFormat(config.video.format)
         grabber.setImageWidth(config.video.frameWidth)
         grabber.setImageHeight(config.video.frameHeight)
         grabber.setFrameRate(config.video.frameRate)
         grabber.start()
         grabber
       }
+    } { grabber =>
+      ZIO.attemptBlocking(grabber.stop()).orDie
+    }
 
-      converter = new Java2DFrameConverter()
+    val converterResource = ZIO.acquireRelease(
+      ZIO.attemptBlocking(new Java2DFrameConverter())
+    )(converter => ZIO.attemptBlocking(converter.close()).orDie)
 
-      // ZStream to process frames and send them to Kafka
-      frameStream = ZStream.unfold(()) { _ =>
-        val frame = grabber.grab()
-        if (frame != null) {
-          val startTime = java.lang.System.nanoTime()
-          val bufferedImage = converter.convert(frame)
-          val byteArray = new Array[Byte](bufferedImage.getWidth * bufferedImage.getHeight * 3)
-          val raster = bufferedImage.getRaster
-          raster.getDataElements(0, 0, bufferedImage.getWidth, bufferedImage.getHeight, byteArray)
-
-          // Update Prometheus metrics with application label
-          framesProduced.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
-          frameSize.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).set(byteArray.length)
-          frameProductionTime
-            .labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE)
-            .observe((java.lang.System.nanoTime() - startTime) / 1e9)
-
-          Some((byteArray, ()))
-        } else {
-          None
-        }
-      }
-
-      // Process the stream and send frames to Kafka
-      _ <- frameStream
-        .mapZIO { byteArray =>
-          val record = new ProducerRecord[Array[Byte], Array[Byte]](config.kafka.topic, byteArray)
-          producer.produce(record, Serde.byteArray, Serde.byteArray)
-            .catchAll { ex =>
-              kafkaProducerErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
-              ZIO.attempt(println(s"Error sending frame to Kafka: ${ex.getMessage}"))
-            }
-        }
-        .runDrain
-    } yield ()
+    grabberResource.zip(converterResource)
   }
 
-  // Main loop to process video files
-  def processVideoFiles(producer: Producer, config: AppConfig): ZIO[Any, Throwable, Unit] = {
-    for {
-      _ <- ZIO.attempt(println("Checking for video files..."))
-      videoDir = new Path(config.hdfs.videoPath)
-      videoFiles <- ZIO.attemptBlocking {
-        val conf = new Configuration()
-        conf.set("fs.defaultFS", config.hdfs.uri)
-        conf.addResource(new Path("/etc/hadoop/core-site.xml"))
-        conf.addResource(new Path("/etc/hadoop/hdfs-site.xml"))
-        val fs = FileSystem.get(new URI(config.hdfs.uri), conf)
-        fs.listStatus(videoDir).filter(_.isFile)
-      }
-      _ <- if (videoFiles.isEmpty) {
-        ZIO.attempt(println("No video files found. Waiting for 5 seconds...")) *> ZIO.sleep(5.seconds)
-      } else {
-        ZIO.attempt(println(s"Found ${videoFiles.length} video files. Processing...")) *>
-          ZIO.foreachDiscard(videoFiles.sortBy(_.getPath.getName)) { fileStatus =>
-            val videoPath = fileStatus.getPath
-            if (videoPath.getName.matches("video_\\d{2}\\.mp4")) {
-              processVideoFrames(producer, videoPath, config)
-            } else {
-              ZIO.unit // Skip files that don't match the pattern
+  // Process a single video file
+  def processVideoFile(
+    producer: Producer,
+    fs: FileSystem,
+    videoPath: Path,
+    config: AppConfig,
+    logger: Logger
+  ): ZIO[Scope, Throwable, Unit] = {
+    ZIO.scoped {
+      hdfsInputStream(fs, videoPath).flatMap { inputStream =>
+        ffmpegResources(inputStream, config).flatMap { case (grabber, converter) =>
+          val frameCounter = new AtomicInteger(0)
+
+          ZStream.unfoldZIO(()) { _ =>
+            ZIO.attemptBlocking {
+              val frame = grabber.grab()
+              if (frame != null) {
+                val startTime = java.lang.System.nanoTime()
+                try {
+                  val bufferedImage = converter.convert(frame)
+                  val byteArray = new Array[Byte](bufferedImage.getWidth * bufferedImage.getHeight * 3)
+                  bufferedImage.getRaster.getDataElements(
+                    0, 0, bufferedImage.getWidth, bufferedImage.getHeight, byteArray
+                  )
+
+                  // Update metrics
+                  framesProduced.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
+                  frameSize.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).set(byteArray.length)
+                  frameProductionTime
+                    .labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE)
+                    .observe((java.lang.System.nanoTime() - startTime) / 1e9)
+
+                  val newCount = frameCounter.incrementAndGet()
+                  if (newCount % 100 == 0) {
+                    logger.debug(s"Processed $newCount frames from ${videoPath.getName}")
+                    java.lang.System.gc()
+                  }
+
+                  Some((byteArray, ()))
+                } catch {
+                  case ex: Exception =>
+                    logger.error(s"Error processing frame: ${ex.getMessage}")
+                    frameProductionErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
+                    Some((Array.emptyByteArray, ()))
+                } finally {
+                  frame.close()
+                }
+              } else {
+                None
+              }
             }
           }
+          .filter(_.nonEmpty)
+          .mapZIO { byteArray =>
+            val record = new ProducerRecord[Array[Byte], Array[Byte]](
+              config.kafka.topic, 
+              Array.emptyByteArray, 
+              byteArray
+            )
+            producer.produce(record, Serde.byteArray, Serde.byteArray)
+              .catchAll { ex =>
+                kafkaProducerErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc()
+                ZIO.attempt(logger.error(s"Error sending frame to Kafka: ${ex.getMessage}"))
+              }
+          }
+          .runDrain
+        }
+      }.catchAll { ex =>
+        ZIO.attempt(logger.error(s"Error processing video ${videoPath.getName}: ${ex.getMessage}")) *>
+          ZIO.succeed(hdfsReadErrors.labels(APPLICATION_VALUE, INSTANCE_VALUE, JOB_VALUE).inc())
       }
-      _ <- processVideoFiles(producer, config) // Recursively call to continue processing
+    }
+  }
+
+  // Main processing loop with graceful shutdown
+  def processVideoFiles(
+    producer: Producer,
+    fs: FileSystem,
+    config: AppConfig,
+    logger: Logger,
+    shutdownSignal: Promise[Nothing, Unit]
+  ): ZIO[Scope, Throwable, Unit] = {
+    def listVideoFiles: ZIO[Any, Throwable, List[Path]] = ZIO.attemptBlocking {
+      val videoDir = new Path(config.hdfs.videoPath)
+      fs.listStatus(videoDir).toList
+        .filter(_.isFile)
+        .filter(_.getPath.getName.matches("video_\\d{2}\\.mp4"))
+        .sortBy(_.getPath.getName)
+        .map(_.getPath)
+    }
+
+    def processBatch(files: List[Path]): ZIO[Scope, Throwable, Unit] = {
+      ZIO.foreachDiscard(files) { file =>
+        processVideoFile(producer, fs, file, config, logger)
+          .catchAll(e => ZIO.attempt(logger.error(s"Failed to process ${file.getName}: ${e.getMessage}")))
+      }
+    }
+
+    for {
+      _ <- ZIO.attempt(logger.info("Starting video processing loop"))
+      files <- listVideoFiles
+      _ <- if (files.isEmpty) {
+        ZIO.attempt(logger.warn("No video files found. Waiting...")) *> 
+        ZIO.sleep(5.seconds) *>
+        ZIO.attempt(java.lang.System.gc())
+      } else {
+        ZIO.attempt(logger.info(s"Found ${files.size} video files to process")) *> 
+        processBatch(files)
+      }
+      shouldContinue <- shutdownSignal.isDone.map(!_)
+      _ <- if (shouldContinue) processVideoFiles(producer, fs, config, logger, shutdownSignal) 
+          else ZIO.attempt(logger.info("Shutting down video processing"))
     } yield ()
   }
 
-  // Main entry point
-  override def run: ZIO[Any, Throwable, Unit] = {
-    for {
-      config <- ZIO.service[AppConfig]
-      _ <- ZIO.attempt {
-        // Start Prometheus HTTP server
-        DefaultExports.initialize()
-        new HTTPServer(9084)
-        println("Prometheus HTTP server started on port 9084")
-      }
-      producer <- ZIO.service[Producer]
-      _ <- processVideoFiles(producer, config) // Start the main processing loop
-    } yield ()
-  }.provide(
-    configLayer,
-    ZLayer.scoped {
-      for {
-        config <- ZIO.service[AppConfig]
-        producer <- Producer.make(ProducerSettings(List(config.kafka.bootstrapServers)))
-      } yield producer
+  // Main entry point with proper resource cleanup
+  override def run: ZIO[ZIOAppArgs with Scope, Any, Any] = {
+    val logger = LoggerFactory.getLogger(getClass)
+
+    ZIO.scoped {
+      (ZIO.attempt(FFmpegLogCallback.set()) *>
+        ZIO.attempt(DefaultExports.initialize()) *>
+        ZIO.acquireRelease(
+          ZIO.attempt(new HTTPServer(9084))
+        )(server => ZIO.attempt(server.stop()).orDie)
+    )}.flatMap { _ =>
+      ZIO.scoped {
+        Promise.make[Nothing, Unit].flatMap { shutdownSignal =>
+          ZIO.addFinalizer(
+            ZIO.succeed(logger.info("Shutdown signal received")) *>
+            shutdownSignal.succeed(()).unit
+          ) *>
+          ZIO.serviceWithZIO[AppConfig] { config =>
+            ZIO.scoped {
+              for {
+                fs <- fileSystem(config)
+                producer <- Producer.make(ProducerSettings(List(config.kafka.bootstrapServers)))
+                _ <- processVideoFiles(
+                  producer = producer,
+                  fs = fs,
+                  config = config,
+                  logger = logger,
+                  shutdownSignal = shutdownSignal
+                )
+              } yield ()
+            }.catchAll(e => ZIO.succeed(logger.error(s"Fatal error: ${e.getMessage}")))
+          }
+        }
+      }.provideSome[Scope](configLayer)
     }
-  )
+  }
 }
